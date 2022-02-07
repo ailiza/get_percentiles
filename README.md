c0fae957f08bbe4b519159722a3020cbd2854a14
A Iliza <annie.ilizarov@gmail.com>
2022-02-07 11:24:13 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,14 +1,31 @@
-# GetPercentiles by Annie Ilizarov
+# GetPercentiles by Annie Ilizarov, Henry Xie & Simple Fractal, LLC.
 
-To run this web app, type:
+## Introduction
+We created a React app for benchmarking a software engineer's coding and communication skills against engineers that work at similar companies and with the same title (e.g., Junior Engineer)
+
+## Scope of functionalities 
+- [X] Enter a candidate's ID and returns their percentiles for their coding and communication score compared to other candidates at the same title and at similar companies
+- [X] Enter a new candidate's information (ID, communication score, coding score, title and company ID)
+
+## To Run This Web App, type:
 
 ```
 npm install
 npm start
 ```
 
-To run the tests, type:
+## To Run The Test(s), type:
 
 ```
 npm run test
 ```
+
+## Stretch Goals
+
+Ensure our app is widely accessible to a broad demographic of users:
+
+- Set up a database for new candidate's information to persist
+- Fetch data from the internet
+- Allow a user to upload their own .csv file
+- Migrate this app to the cloud
+
--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from 'react';
-import Papa from 'papaparse'
-
+import Papa from 'papaparse';
 
 function App({ defaultCompanies, defaultScoreRecords }) {
   const [companies, setCompanies] = useState({ data: [] });
   const [scoreRecords, setScoreRecords] = useState({ data: [] });
-  const [candidateId, setCandidateId] = useState(''); 
+  const [candidateId, setCandidateId] = useState('');
+  const [newCandidate, setNewCandidate] = useState('');
+  
   useEffect(() => {
     async function fetchCsv() {
       const scoreRecordsCsv = await fetch('/score-records.csv');
       const scoreRecordsText = await scoreRecordsCsv.text();
       const scoreRecordsData = Papa.parse(scoreRecordsText, { header: true });
       setScoreRecords(scoreRecordsData);
-      console.log(scoreRecordsData)
+      // console.log(scoreRecordsData)
       const companiesCsv = await fetch('/companies.csv');
       const companiesText = await companiesCsv.text();
       const companiesData = Papa.parse(companiesText, { header: true });
-      console.log(companiesData)
+      // console.log(companiesData)
       setCompanies(companiesData);
 
     };
+
     if (defaultCompanies) setCompanies(defaultCompanies);
     if (defaultScoreRecords) setScoreRecords(defaultScoreRecords);
     else fetchCsv();
+
   }, [setScoreRecords, setCompanies, defaultScoreRecords, defaultCompanies]);
 
   function getSimilarCompanies(companyId) {
@@ -36,11 +38,11 @@ function App({ defaultCompanies, defaultScoreRecords }) {
     const candidate = scoreRecords.data.find(c => c.candidate_id === candidateId) || {};
     const sameTitleCandidates = scoreRecords.data.filter(c => c.title === candidate.title);
     const similarCompanyIds = getSimilarCompanies(candidate.company_id);
-    console.log("similarCompanyIds", similarCompanyIds)
+    // console.log("similarCompanyIds", similarCompanyIds)
     const similarCompanyCandidates = sameTitleCandidates.filter(c => similarCompanyIds.includes(c.company_id));
     const codeRank = similarCompanyCandidates.sort((a,b) => a.coding_score - b.coding_score);
 
-    console.log(codeRank)
+    // console.log(codeRank)
     const codeIdx = codeRank.indexOf(candidate) + 1;
     const codePercentile = (codeIdx / codeRank.length) * 100;
 
@@ -68,6 +70,14 @@ function App({ defaultCompanies, defaultScoreRecords }) {
             communicationPercentile > 0 ? communicationPercentile.toFixed(2) + '%' : 'Type A Valid Candidate ID'
           }
         </p>
+        <input type='text' placeholder='Candidate ID' value={newCandidate.candidate_id} onChange={e => setNewCandidate({ ...newCandidate, candidate_id: e.target.value })} />
+        <input type='text' placeholder='Communication Score' value={newCandidate.communication_score} onChange={e => setNewCandidate({ ...newCandidate, communication_score: e.target.value })} />
+        <input type='text' placeholder='Coding Score' value={newCandidate.coding_score} onChange={e => setNewCandidate({ ...newCandidate, coding_score: e.target.value })} />
+        <input type='text' placeholder='Title' value={newCandidate.title} onChange={e => setNewCandidate({ ...newCandidate, title: e.target.value })} />
+        <input type='text' placeholder='Company ID' value={newCandidate.company_id} onChange={e => setNewCandidate({ ...newCandidate, company_id: e.target.value })} />
+        <button onClick={() => {
+          setScoreRecords({ ...scoreRecords, data: [ ...scoreRecords.data, newCandidate ]});
+        }}>Add New Candidate</button>
       </header>
     </div>
   );
